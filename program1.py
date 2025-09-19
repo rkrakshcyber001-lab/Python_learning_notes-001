e1e219d6d514aa45be8f95a3a622fed1c4666b5b
Rakshitha-D-H <>
2025-09-19 11:37:59 +0530

    I have added my previous programs

--- program1.py
+++ program1.py
@@ -1,1 +1,84 @@
-print("Hello, World!!")
+"""
+#Program 1
+
+print("hello World")
+
+#Program 2
+
+print("hello \nworld") #usage of escape sequence
+
+# Program 3
+
+print("Hey", end="009") 
+print("Hello")
+
+# Program 4
+
+print("hey", 4,sep="*") #o/p: hey*4
+
+
+# Program 5
+a=2
+b=4
+
+print(f"addition of the two numbers {a} and  {b}:",a+b )
+print(f"subtraction of the two numbers {a} and  {b}:",a-b )
+print(f"multiplication of the two numbers {a} and  {b}:",a*b )
+print(f"division of the two numbers {a} and  {b}:",a/b )
+
+
+# Program 6
+a = "1"
+b = "2"
+print(int(a) + int(b)) # Type Casting - explicit conversion (user manually does the type casting)
+
+# Program 7
+
+a = 7.6 #float
+b = 4   #int
+c=a+b
+
+print(c) #float cz its implicit conversion did by the python interpreter to carry out the operation based on the order level of data type
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Program 8  
+a = int(input("enter the 1st number:"))
+b = int(input("enter the 2nd number:"))
+print(a + b)
+
+# Program 9 - Accessing characters of a string
+name = "raksha"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+# Program 10 - Looping through string
+name = "kamala"
+for character in name:
+    print(character)
+
+# Program 11 - Finding the length of a string
+container = "boxing"
+print(f"the length of the word {container} is" ,len(container))
+
+# Program 12 - Slicing of characters in a string
+string_slicing = "HappyBirthday" #for slicing the indexing will take the inputs from 0 to n-1
+print(string_slicing[0:5])
+print(string_slicing[:5]) # python will automatically consider the index from 0 without mentioning it
+print(string_slicing[5:14])
+print(len(string_slicing))
+print(string_slicing[:]) # prints the whole string
+print(string_slicing[0:len(string_slicing)-8])
+print(string_slicing[0:-8]) #negative slicing
+print(string_slicing[-8:-3]) # output: Birth
+"""
+
+# Program 13 - String methods and operations (Strings are immutable)
+instrument = "Flute"
+print(instrument.upper())
+print(instrument.lower())
